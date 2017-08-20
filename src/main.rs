42834289303b0a699cc25ee52c03a4c553ff3e2c
letusfly85 <letusfly95@gmail.com>
2017-08-21 01:05:49 +0900

    add body parser

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,40 @@ extern crate persistent;
 #[macro_use]
 extern crate serde_derive;
 
+extern crate serde;
+extern crate serde_json;
+
 use persistent::Read;
 use iron::status;
 use iron::prelude::*;
 
+#[derive(Debug, Clone, Deserialize)]
+pub struct MyStructure {
+    a: String,
+    b: Option<String>,
+}
+
 fn log_body(req: &mut Request) -> IronResult<Response> {
     let body = req.get::<bodyparser::Raw>();
+    match body {
+        Ok(Some(body)) => println!("Read body:\n{}", body),
+        Ok(None) => println!("No body"),
+        Err(err) => println!("Error: {:?}", err),
+    }
+
+    let json_body = req.get::<bodyparser::Json>();
+    match json_body {
+        Ok(Some(json_body)) => println!("Parsed body:\n{:?}", json_body),
+        Ok(None) => println!("No body"),
+        Err(err) => println!("Error: {:?}", err),
+    }
+
+    let struct_body = req.get::<bodyparser::Struct<MyStructure>>();
+    match struct_body {
+        Ok(Some(struct_body)) => println!("Parsed body:\n{:?}", struct_body),
+        Ok(None) => println!("No body"),
+        Err(err) => println!("Error: {:?}", err),
+    }
 
     Ok(Response::with(status::Ok))
 }
